f105fe756cca0c9f4f821f7417ca1f02bc2229ca
tomoyuki@CentOS6X64 <hzd02765@nifty.com>
2013-06-20 18:55:00 +0900

    dotinstall #23

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TodoBord::Application.config.secret_token = '819176f662991c8226155ef4cc5e2e38688b864c238a7482068995e3d44b42eaab6f0f274b613b3d62dd2be6abae86fd67210e0163d53f7053a8d5471bd7db2a'
