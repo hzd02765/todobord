fe4f1df0836532dbf05aabb49bf5daa5f0a09eb0
tomoyuki@fedora18 <hzd02765@nifty.com>
2013-06-21 07:22:53 +0900

    とりあえず作った

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,5 @@
+# coding: utf-8
+
 class PostsController < ApplicationController
   def index
     @posts = Post.all
@@ -6,4 +8,39 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params[:post])
+    if @post.save
+      # redirect_to posts_path
+      redirect_to posts_path, notice: '作成されました！'
+    else
+      render action: 'new'
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update_attributes(params[:post])
+      redirect_to posts_path, notice: '更新されました！'
+    else
+      render action: 'edit'
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    #redirect_to posts_path
+    render json: {post: @post}
+  end
+
 end
